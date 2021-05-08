d869f813206264bf8e35acff436edd6513c1a988
Carlos Barreiro <carlosbarreiro2604@gmail.com>
2021-05-08 18:30:17 +0100

    home and calculator updates

--- project/src/app/components/caculator/caculator.component.ts
+++ project/src/app/components/caculator/caculator.component.ts
@@ -11,11 +11,19 @@ export class CaculatorComponent implements OnInit {
   valorY=0;
   sinal:string;
   resultado=0;
+  btn1:number;
+  btn2:string;
+  btn3:number;
+  resultT=0;
 
   constructor() { }
 
   calcula(){
     this.resultado = eval(this.valorX + this.sinal + this.valorY);
+  }
+
+  calculat(){
+    this.resultT = eval(this.btn1 + this.btn2 + this.btn3);
   }
 
   ngOnInit(): void {
